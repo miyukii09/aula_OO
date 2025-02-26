b9ae4f2a5ae4a512c8aed9f67340142104c97679
Yukii <136659293+miyukii09@users.noreply.github.com>
2025-02-26 12:23:23 -0300

    update26/02/25

---
+++ Aula2_OO/src/Principal.java
@@ -0,0 +1,8 @@
+public class Principal 
+{
+    public static void main(String[] args) {
+        Professor obijeto1, obijeto2;
+       obijeto1 = new Professor();
+       System.out.println(obijeto1);
+    }
+}
---
+++ Aula2_OO/src/Professor.java
@@ -0,0 +1,57 @@
+public class Professor 
+{
+    private String nome;
+    private int quantidadeAulas;
+    private double salario;
+
+    public Professor() 
+    {
+        nome            = "";
+       quantidadeAulas  = 0;
+       salario          = 0;
+    }
+    
+    public Professor(String name, int quantidadeAulas)
+    {
+        this.nome            = nome;
+        this.quantidadeAulas = quantidadeAulas;
+        this.salario         = 0;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidadeAulas() {
+        return quantidadeAulas;
+    }
+
+    public void setQuantidadeAulas(int quantidadeAulas) {
+        this.quantidadeAulas = quantidadeAulas;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    @Override
+    public String toString() {
+        return "\nNome  " + nome + 
+               "\nAulas = " + quantidadeAulas + 
+               "\nSalario = " + salario;
+    }
+    
+
+    public void calcularSalario()
+    {
+        salario = quantidadeAulas *4.5 * 40;
+    }
+    
+    
+    
+}
+
