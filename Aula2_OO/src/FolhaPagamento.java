7a9e7a0ac84e74b495b3ffbad2f7715dce1eb1e9
Yukii <136659293+miyukii09@users.noreply.github.com>
2025-02-27 12:24:33 -0300

    update27/20/25

--- Aula2_OO/src/FolhaPagamento.java
+++ Aula2_OO/src/FolhaPagamento.java
@@ -1,12 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.util.List;
+import java.util.ArrayList;
 
-/**
- *
- * @author aluno
- */
 public class FolhaPagamento {
+    private ArrayList<Professor> lista;
     
+    public FolhaPagamento()
+    {
+        lista = new ArrayList<>();
+    }
+
+    public ArrayList<Professor> getLista() {
+        return lista;
+    }
+    
+    public void contratarPorfessor(Professor objeto)
+    {
+        lista.add(objeto);
+    }
+    public void contratarPorfessor(String nome, int qtdAulas)
+    {
+        lista.add( new Professor(nome, qtdAulas));
+    }
+    public void demitirProfessor(int i)
+    {
+        lista.remove(i);
+    }
+    
+    public double calcularSalarioTotal()
+    {
+        double total = 0;
+        
+        for (Professor objeto : lista)
+        {
+            total = total +objeto.getSalario();
+        }
+    }
 }
