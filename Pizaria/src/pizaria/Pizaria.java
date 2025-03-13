3e90f7e454031efe1926f491e42dfa614672063a
Yukii <136659293+miyukii09@users.noreply.github.com>
2025-03-13 12:18:51 -0300

    update 13/03/2025

---
+++ Pizaria/src/pizaria/Pizaria.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pizaria;
+
+/**
+ *
+ * @author aluno
+ */
+public class Pizaria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Pizzaria/src/Pizzas.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class Pizzas {
+    private String nome;
+    private double preco;
+    public Pizzas()
+    {
+        nome    = "";
+        preco   = 0;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    
+}
